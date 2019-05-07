ab60a72f4850f0193f5c370805845872233ed8af
Firelonze <26387@ma-web.nl>
2019-05-07 23:54:05 +0200

    Requirements fixed

---
+++ Assets/Scripts/RequirementsScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RequirementsScript : MonoBehaviour
+{
+    private float timeLeft;
+    public bool Alive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //for loop
+        //while loop
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (Alive == true)
+        {
+            timeLeft += Time.deltaTime;
+        }
+    }
+}
